fc7d099d535de0c4fbfc3dc4a9700a289728b461
Lucas Barros <luveslucas@yahoo.com.br>
2020-07-30 11:08:07 -0300

    montando anuncio

--- src/components/anuncio/index.js
+++ src/components/anuncio/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./style.css";
-import anuncio from "../anuncio/comprimento.jpg";
+import Anuncio from "../anuncio/comprimento.jpg";
 
 class Cartaz extends React.Component {
   render() {
     return (
       <div className="container">
+        <div className="titulo">
+          <h2>Conheça nossa nova linha de bolos de rolo</h2>
+        </div>
         <div className="anuncio">
-          <img src={anuncio} alt="anuncio" width="1000" height="500" />
+          <img src={Anuncio} alt="anuncio" width="1000" height="500" />
+        </div>
+        <div className="info">
+          <h3>Agora com entregas para todo sudeste</h3>
         </div>
       </div>
     );
