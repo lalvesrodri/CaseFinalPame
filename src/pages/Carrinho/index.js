b38312efa8fbac868f30b41266cb995a48201fb4
Lucas Barros <luveslucas@yahoo.com.br>
2020-07-30 23:01:33 -0300

    adicionando os produtos carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "./style.css";
 import Header from "../../components/header/index";
 import Futter from "../../components/futter/index";
+import Foto from "../../components/anuncio/comprimento.jpg";
 
 class Carrinho extends React.Component {
   render() {
@@ -14,9 +15,12 @@ class Carrinho extends React.Component {
           <div className="titulo">
             <h2 className="title">Meus produtos no Carrinho</h2>
           </div>
-          <div className="produtos"></div>
-          <div className="fotos"></div>
-          <div className="descricao"></div>
+          <div className="produtos">
+            <div className="fotos">
+              <img src={Foto} alt="anuncio" width="100" height="50" />
+            </div>
+            <div className="descricao">Texto do produto</div>
+          </div>
         </div>
         <Futter />
       </>
